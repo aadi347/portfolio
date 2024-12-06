cb82326b8ed7ce753a4ab5f4a3e20c6c32098600
Aditya Kumar <aadi678a@gmail.com>
2024-12-06 22:34:34 +0530

    update the home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import Hero from './Hero'
-import Blog from './Blog'
-import Contact from './Contact'
-import About from './About'
-import Projects from './Projects'
-import Skills from './Skills'
+// import Hero from './Hero'
+// import Blog from './Blog'
+// import Contact from './Contact'
+// import About from './About'
+// import Projects from './Projects'
+// import Skills from './Skills'
 
 
 const Home = () => {
   return (
-    <div className='bg-red-600'>
-        <Hero />
-        <Blog />
-        <Contact />
-        <About />
-        <Projects />
-        <Skills />
-    </div>
+<div className="grid grid-cols-3 xl:grid-cols-3 gap-4 px-16 py-4">
+    <div className="w-full h-56 bg-blue-600 rounded-3xl"></div>
+    <div className="w-full h-56 bg-blue-900 rounded-3xl col-span-2"></div>
+    <div className="w-full h-72 bg-blue-900 rounded-3xl col-span-2"></div>
+    <div className="w-full h-72 bg-blue-600 rounded-3xl"></div>
+    <div className="w-full h-24 bg-blue-600 rounded-3xl"></div>
+    <div className="w-full h-24 bg-blue-900 rounded-3xl col-span-2"></div>
+</div>
+
+
   )
 }
 
